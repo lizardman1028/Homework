585ba624a0b07e5ec878b348a4055f3bf761c15a
Leyth Toubassy <lizardman1028@gmail.com>
2019-02-20 17:54:33 -0800

    Testing state name

--- AppHW/App.js
+++ AppHW/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+yote = {
+  testVar: 10
+}
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Yeetus der Fötus</Text>
+        <Text>{this.yote.testVar}</Text>
       </View>
     );
   }
