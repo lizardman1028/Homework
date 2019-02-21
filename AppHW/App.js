5809dfea2d4d99510d5cb1b794e31840a31cc28b
Leyth Toubassy <lizardman1028@gmail.com>
2019-02-20 19:54:35 -0800

    Buttond

--- AppHW/App.js
+++ AppHW/App.js
@@ -1,46 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-// export default class App extends React.Component {
-// state = {testVar: 9}
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Yeetus der Fötus</Text>
-//         <Text>{this.yote.testVar}</Text>
-//       </View>
-//     );
-//   } 
-// }
+export default class App extends React.Component {
+state = {testVar: 9}
+handleIncrement = () => {
+  this.setState({ count: this.state.count + 1 })
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#ffff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-class App extends React.Component {
-
-  state = { count: 0 }
-  
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-  
-  handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-    render() {
-      return (
+handleDecrement = () => {
+  this.setState({ count: this.state.count - 1 })
+}
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Yeetus der Fötus</Text>
+        <Text>{this.state.testVar}</Text>
         <div>
-          <div>
-            {this.state.count}
-          </div>
           <button onClick={this.handleIncrement}>Increment by 1</button>
           <button onClick={this.handleDecrement}>Decrement by 1</button>
         </div>
-      )
-    }
+      </View>
+    );
   }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
