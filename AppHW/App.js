428aab923e6f3dd9bb2c04409345dc81ee26c7b3
Leyth Toubassy <lizardman1028@gmail.com>
2019-02-20 19:51:37 -0800

    Imports

--- AppHW/App.js
+++ AppHW/App.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 // export default class App extends React.Component {
 // state = {testVar: 9}
